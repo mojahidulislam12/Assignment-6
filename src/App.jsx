1c09ff3c1556461c5d695d40776acf60126e4afe
Sagor <mojahidulislamsagor1234@gmail.com>
2026-03-29 23:55:00 +0600

    All Count Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Active from "./Components/Active/Active";
 import Banner from "./Components/Banner/Banner";
 import NavBar from "./Components/NaBar/NavBar";
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <NavBar></NavBar>
       <Banner></Banner>
+      <Active></Active>
     </>
   );
 }
---
+++ src/Components/Active/Active.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Active = () => {
+  return (
+    <div className="bg-[#4F39F6] h-61.75 text-white ">
+      <div className="container mx-auto flex justify-around items-center  ml-50">
+        <div className="my-15">
+          <h1 className="font-extrabold text-[60px]">50K+</h1>
+          <p className="font-medium text-[24px] mt-2 opacity-60">
+            Active Users
+          </p>
+        </div>
+        <div>
+          <h1 className="font-extrabold text-[60px]">200+</h1>
+          <p className="font-medium text-[24px] mt-2 opacity-60">
+            Premium Tools
+          </p>
+        </div>
+        <div>
+          <h1 className="font-extrabold text-[60px]">4.9</h1>
+          <p className="font-medium text-[24px] mt-2 opacity-60">Rating</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Active;
