4ee7b85e91933c8060cf7b3f2b232b7277a48b05
Sagor <mojahidulislamsagor1234@gmail.com>
2026-03-30 09:21:22 +0600

    Card Section

--- src/Components/AllCard/AllCard.jsx
+++ src/Components/AllCard/AllCard.jsx
@@ -5,10 +5,10 @@ const AllCard = ({ allCards }) => {
   const cards = use(allCards);
   //console.log(cards);
   return (
-    <div>
-      <div>
-        {cards.map((card) => (
-          <Card card={card}></Card>
+    <div className="container mx-auto mt-10 ">
+      <div className="grid grid-cols-3 gap-7.5">
+        {cards.map((card, index) => (
+          <Card key={index} card={card}></Card>
         ))}
       </div>
     </div>
