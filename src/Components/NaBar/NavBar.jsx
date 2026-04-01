dfb4716108481fd3fc503ae09a6c5eeb72a9b1c4
Sagor <mojahidulislamsagor1234@gmail.com>
2026-04-01 16:07:26 +0600

    Commit

--- src/Components/NaBar/NavBar.jsx
+++ src/Components/NaBar/NavBar.jsx
@@ -7,30 +7,32 @@ const NavBar = ({ selectedCards }) => {
   return (
     <div>
       <div className="">
-        <div className="flex justify-around  items-center navbar bg-base-100 shadow-sm ">
-          <span
-            onClick={() => setOpen(!open)}
-            className="flex justify-center items-center gap-5"
-          >
-            {open === true ? (
-              <X className="md:hidden"></X>
-            ) : (
-              <Menu className="md:hidden"></Menu>
-            )}
-            <img src={img1} className=" md:5" alt="" />
-            <ul
-              className={`md:hidden absolute duration-1000 p-3
+        <div className="max-auto flex justify-around  items-center navbar bg-base-100 shadow-sm ">
+          <div>
+            {" "}
+            <span
+              onClick={() => setOpen(!open)}
+              className="flex justify-center items-center gap-5"
+            >
+              {open === true ? (
+                <X className="md:hidden"></X>
+              ) : (
+                <Menu className="md:hidden"></Menu>
+              )}
+              <img src={img1} className="" alt="" />
+              <ul
+                className={`md:hidden absolute duration-1000 p-3
             ${open ? "top-16" : "-top-40"}
              bg-amber-200 -ml-30`}
-            >
-              <li>Products</li>
-              <li>Features</li>
-              <li>Pricing</li>
-              <li>Testimonials</li>
-              <li>FAQ</li>
-            </ul>
-          </span>
-          <div></div>
+              >
+                <li>Products</li>
+                <li>Features</li>
+                <li>Pricing</li>
+                <li>Testimonials</li>
+                <li>FAQ</li>
+              </ul>
+            </span>
+          </div>
           <div className="">
             <ul className="md:flex  hidden menu gap-8 menu-horizontal px-1 font-semibold text-5 text-[#101727">
               <li>Products</li>
